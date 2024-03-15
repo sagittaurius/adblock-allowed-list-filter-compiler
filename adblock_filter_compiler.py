744b7355988da73c97acb17042b656138717cfa9
sagittaurius <44752104+sagittaurius@users.noreply.github.com>
2024-03-15 23:10:33 +0700

    Update adblock_filter_compiler.py

--- adblock_filter_compiler.py
+++ adblock_filter_compiler.py
@@ -85,8 +85,17 @@ def process_filter_content_with_allowlist_domains(filter_content: List[str], all
 def generate_combined_filter_file():
     """Main function to fetch blocklists and generate a combined filter."""
     blocklist_urls = [
-
-        "https://github.com/fabriziosalmi/blacklists/releases/download/latest/blacklist.txt"
+       "https://phishing.army/download/phishing_army_blocklist_extended.txt",
+        "https://malware-filter.gitlab.io/malware-filter/phishing-filter.txt",
+        "https://blocklistproject.github.io/Lists/alt-version/ransomware-nl.txt",
+        "https://blocklistproject.github.io/Lists/alt-version/abuse-nl.txt",
+        "https://blocklistproject.github.io/Lists/alt-version/phishing-nl.txt",
+        "https://blocklistproject.github.io/Lists/alt-version/malware-nl.txt", 
+        "https://raw.githubusercontent.com/ShadowWhisperer/BlockLists/master/Lists/Malware",
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/malware",
+        "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/Phishing-Angriffe",
+        "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/popupads.txt",
+        "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/tif.txt"
     ]
     allowlist_urls = ["https://raw.githubusercontent.com/sagittaurius/main/main/whitelist"]
 
