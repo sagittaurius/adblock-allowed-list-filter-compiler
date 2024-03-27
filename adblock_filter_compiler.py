463c837c7db4b7f4c0ccddac24c0c37d4705e4fc
sagittaurius <44752104+sagittaurius@users.noreply.github.com>
2024-03-27 22:21:15 +0700

    Update adblock_filter_compiler.py

--- adblock_filter_compiler.py
+++ adblock_filter_compiler.py
@@ -85,7 +85,8 @@ def process_filter_content_with_allowlist_domains(filter_content: List[str], all
 def generate_combined_filter_file():
     """Main function to fetch blocklists and generate a combined filter."""
     blocklist_urls = [
-       "https://phishing.army/download/phishing_army_blocklist_extended.txt",
+        "https://hblock.molinero.dev/hosts_adblock.txt",
+        "https://phishing.army/download/phishing_army_blocklist_extended.txt",
         "https://blocklistproject.github.io/Lists/alt-version/ransomware-nl.txt",
         "https://blocklistproject.github.io/Lists/alt-version/malware-nl.txt", 
         "https://raw.githubusercontent.com/ShadowWhisperer/BlockLists/master/Lists/Malware",
